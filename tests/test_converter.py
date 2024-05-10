be64adc4ee0a9c69843fcb6a36ffdcb9d73ccdbf
Ilya Nikitin <ilya.i.nikitin@proton.me>
2024-05-10 22:17:19 +0300

    Fix test file path

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -10,7 +10,7 @@ from webanno2spacy import convert_rows
 @pytest.mark.parametrize("model_name", ["ru_core_news_lg"])
 def test_convert_rows(model_name):
     nlp = spacy.load(model_name)
-    input_text_file = Path(model_name) / "Путь-Королей-00.txt"
+    input_text_file = Path(__file__).parent / model_name / "Путь-Королей-00.txt"
     input_webanno_file = input_text_file.with_suffix(".tsv")
     text = input_text_file.read_text()
     with open(input_webanno_file) as fd:
