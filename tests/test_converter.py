1ad51d0bf317ac6bed8059e3b7794fdfbfea5c43
Ilya Nikitin <ilya.i.nikitin@proton.me>
2024-05-10 22:05:06 +0300

    Add simple test

---
+++ tests/test_converter.py
@@ -0,0 +1,20 @@
+import csv
+from pathlib import Path
+
+import pytest
+import spacy
+
+from webanno2spacy import convert_rows
+
+
+@pytest.mark.parametrize("model_name", ["ru_core_news_lg"])
+def test_convert_rows(model_name):
+    nlp = spacy.load(model_name)
+    input_text_file = Path(model_name) / "Путь-Королей-00.txt"
+    input_webanno_file = input_text_file.with_suffix(".tsv")
+    text = input_text_file.read_text()
+    with open(input_webanno_file) as fd:
+        rd = csv.reader(fd, delimiter="\t", quotechar='"')
+        doc = convert_rows(nlp, text, rd)
+
+    assert len(doc._.rel) == 231
